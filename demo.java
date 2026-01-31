751b3b0b02372e40714d1c74521deb21bcf44086
18874897593-tech <18874897593@163.com>
2026-01-31 23:38:48 +0800

    demo

---
+++ demo.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       // 先对数组进行排序
+       Arrays.sort(strs);
+
+    }
+}
+
